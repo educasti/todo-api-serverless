141332a29d66644882d5f866e0fd8305679b41db
Eduardo Castillo <educasti@gmail.com>
2026-03-29 22:20:14 -0500

    feat: configure Lambda handler with API Gateway HTTP v2 support

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,18 @@
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./index');
 
-exports.handler = serverlessExpress({ app });
+let serverlessExpressInstance;
+
+exports.handler = async (event, context) => {
+  if (!serverlessExpressInstance) {
+    serverlessExpressInstance = serverlessExpress({ app });
+  }
+
+  // Elimina el prefijo /prod del path
+  if (event.rawPath) {
+    event.rawPath = event.rawPath.replace(/^\/prod/, '') || '/';
+    event.requestContext.http.path = event.rawPath;
+  }
+
+  return serverlessExpressInstance(event, context);
+};
